1a1468210878072011e810395b5d91cb5959c91c
Jun <lanjunq@gmail.com>
2022-03-13 23:40:47 +0800

    init

--- resource_monitor.py
+++ resource_monitor.py
@@ -12,7 +12,7 @@ class CPU(Thread):
         self.setDaemon(True)
         self.total_cpu_usage = ''
         self.top = ''
-        self.cpu_per_pid = {}
+        self.cpu_per_process = {}
         self.record_time = record_time
         self.step_time = step_time
         self.running = True
@@ -52,8 +52,27 @@ class CPU(Thread):
         else:
             self.total_cpu_usage = ''
 
-    def decode_each_pid_cpu_usage(self) -> str:
-        pass
+    def decode_each_process_cpu_usage_from_top(self) -> str:
+        if self.top:
+            data_lines = self.top.splitlines()[5:]
+            for line in data_lines:
+                #'  1457 system       18  -2  16G 315M 242M S  0.0   8.6   7:15.33 system_server'
+                line = line.strip()
+                line_split = line.split()
+                print(line_split)
+#                 '''
+#                 TODO: 处理异常字符
+#                 ['\x1b[1m', '23808', 'shell', '20', '0', '10G', '3.0M', '1.8M', 'R', '45.7', '0.0', '0:00.11', 'top', '-n', '1', '-m', '500', '-s', '6']
+# ['\x1b[m']
+#                 '''
+#                 row_cpu = line_split[8]
+#                 # 适配args含有空格情况
+#                 # '1057 root         20   0  10G 1.7M 1.5M S  5.7   0.0   0:28.55 msm_irqbalance -f /vendor/etc/msm_irqbalance.conf',
+#                 row_args = ''.join(line_split[11:])
+#                 self.cpu_per_process[row_args] = row_cpu
+            return
+        else:
+            self.cpu_per_process = {}
 
     def run(self) -> None:
         deadtime = time.time() + self.record_time
@@ -62,7 +81,7 @@ class CPU(Thread):
             # 解析总体cpu使用情况
             self.decode_total_cpu_usage_from_top()
             # 解析每个pid cpu使用情况
-
+            self.decode_each_process_cpu_usage_from_top()
             # 等待步进时间step_time进行下次采样
             time.sleep(self.step_time)
         self.running = False
@@ -77,21 +96,24 @@ if __name__ == '__main__':
     cpu = CPU(record_time=record_time, step_time=step_time)
     cpu.start()
 
-    import matplotlib.pyplot as plt
-
-    x = []
-    y = []
+    # import matplotlib.pyplot as plt
+    #
+    # x = []
+    # y = []
 
     deadtime = time.time() + record_time
     while time.time() < deadtime:
         print(f'cpu.total_cpu_usage: {cpu.total_cpu_usage}')
+
+        if cpu.cpu_per_process:
+            for k, v in cpu.cpu_per_process.items():
+                print(k, v)
         time.sleep(step_time)
 
-
-        x.append(time.time())
-        y.append(cpu.total_cpu_usage)
-        plt.clf()
-
-        plt.plot(x, y)
-        plt.pause(0.1)
-        plt.ioff()
+        # x.append(time.time())
+        # y.append(cpu.total_cpu_usage)
+        # plt.clf()
+        #
+        # plt.plot(x, y)
+        # plt.pause(0.1)
+        # plt.ioff()
